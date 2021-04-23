695f8908970fc25868584cf2ef0fde537e6d7984
宁静致远 <578845426@qq.com>
2021-04-23 16:39:28 +0800

    init

---
+++ app/src/main/java/com/bcq/demo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.bcq.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.kit.utils.Logger;
+import com.looper.Material;
+import com.looper.PipelineQueue;
+import com.looper.interfaces.IMaterial;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.text).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                testKit();
+            }
+        });
+    }
+
+    public void testApply() {
+        List<IMaterial> os = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            os.add(new Material("Queue_" + i,2));
+        }
+        PipelineQueue.getQueue().apply(os);
+    }
+
+    public void testKit() {
+        Logger.e("MainActivity");
+    }
+}
